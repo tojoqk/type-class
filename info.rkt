fd63b51f42ec197d3db442661057073180bed669
Masaya Tojo <masaya@tojo.tokyo>
2019-08-25 23:47:33 +0900

    Edit info.rkt

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/type-class.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "Implementation of type class")
+(define version "0.1")
 (define pkg-authors '(tojo))
